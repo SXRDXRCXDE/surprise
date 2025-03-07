f783b952c7e4d4165a7e7031c956df87618e025b
SXRDXR <sardorbek11b@gmail.com>
2025-03-07 23:02:42 +0500

    Initial commt

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header({position}) {
                 <span className={'text-white text-3xl'}>NIGGA ENTERTAINMENT</span>
             </div> :
             <div className={`w-full h-24 border-b border-white flex flex-col items-center justify-center`}>
-                <span className={'text-white text-3xl font-semibold'}>NIGGA ENTERTAINMENT</span>
+                <span className={' text-white text-3xl font-semibold'}>NIGGA ENTERTAINMENT</span>
             </div>
         }
     </>
